b688a0ee3530710ffa54c01d1e6d95b338c7bee4
jenkins <jenkins@regula.by>
2024-08-21 12:48:46 +0300

    Release 7.3.9680

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["FullRFID"]),
     ],
     targets: [
-        .binaryTarget(name: "FullRFID", url: "https://pods.regulaforensics.com/FullRFID/7.2.9111/DocumentReaderCore_fullrfid_7.2.9111.zip", checksum: "5d9c8a2be6671c717f13e70e05891852c92543ce92e4804a71211d78a76ea407"),
+        .binaryTarget(name: "FullRFID", url: "https://pods.regulaforensics.com/FullRFID/7.3.9680/DocumentReaderCore_fullrfid_7.3.9680.zip", checksum: "9b8f8b30a02a4cddf16d54304cc7838cd757216edbc2292de0fa86b416e8037d"),
     ]
 )
